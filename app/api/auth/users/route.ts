75841c2b0e6880427ddf1c2d0bbe470f8c65e97c
SupanutT <supanut.tangsin@gmail.com>
2024-04-20 23:32:30 +0700

    api : get all users

---
+++ app/api/auth/users/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/drizzle/db';
+import { and, eq } from 'drizzle-orm';
+import { chatMessages } from '@/drizzle/schemas/chatMessages';
+import { chatrooms } from '@/drizzle/schemas/chatrooms';
+import { chatroomUsers } from '@/drizzle/schemas/chatroomUsers';
+import { users } from '@/drizzle/schemas/users';
+
+
+export async function GET(req: NextRequest) {
+    try {
+        const allUsers = await db.select({
+            id: users.id,
+            name: users.name
+        }).from(users)
+        return NextResponse.json({
+            success: true,
+            message: 'Successfully get chatroom information',
+            data: allUsers
+        }, { status: 200 })
+    } catch (err) {
+        return NextResponse.json({
+            success: false,
+            message: 'There is an error occurred',
+        }, { status: 400 })
+    }
+}
