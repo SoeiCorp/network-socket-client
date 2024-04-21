11400fa2f904343a78d53ed19aceeb024aa4162d
SupanutT <supanut.tangsin@gmail.com>
2024-04-21 19:28:54 +0700

    feat : private chat socket client

--- components/socket/client.ts
+++ components/socket/client.ts
@@ -1,22 +1,53 @@
 'use client'
 import { Socket, io } from 'socket.io-client'
 
-const socketServerURL = 'https://soei-socket-server.vercel.app/'
+// const socketServerURL = 'https://soei-socket-server.vercel.app/'
+const socketServerURL = 'http://localhost:3001/'
 
-let curChatroomId: string;
-let socket: Socket = io(socketServerURL)
+let curChatroomId: number;
+let socket: Socket;
 
-export function connect(chatroomId: string, senderId: string) {
+export async function connect(chatroomId: number, senderId: number) {
     if (chatroomId === curChatroomId) return;
-    if (socket) {
-        socket.disconnect();
-    }
+    // if (socket) {
+    //     socket.disconnect();
+    // }
+    const res = await fetch('/api/auth/me');
+    const response = await res.json();
+    const { id } = response.data;
     socket = io(socketServerURL, {
         extraHeaders: {
-            'chat-room-id': chatroomId,
-            'user-id': senderId
+            'user-id': id
         }
     })
+    socket.emit('login', id)
+    let data: any;
+    socket.on('users online', (data) => {
+        console.log('users online', data)
+    })
+    socket.on('private message', (senderId, message) => {
+        console.log('Private message sent from:', senderId, message)
+    })
+    socket.on('private message sent', (recipientId, message) => {
+        console.log('Private message sent to:', recipientId, message)
+    })
+    // console.log("data", data)
 
     curChatroomId = chatroomId
+}
+
+export function sendPrivateMessage(recipientId: Number, message: string) {
+    if (socket) {
+        socket.emit('private message', recipientId, message)
+    } else {
+        console.log('socket not found')
+    }
+}
+
+export function disconnectFromSocket() {
+    if (socket) {
+        socket.disconnect()
+    } else {
+        console.log('socket not found')
+    }
 }
