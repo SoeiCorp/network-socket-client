43b5d39e7fc8cba03028bcdfff049db198b06b99
SupanutT <supanut.tangsin@gmail.com>
2024-04-22 04:09:02 +0700

    feat : socket client private and group chatroom

--- components/socket/client.ts
+++ components/socket/client.ts
@@ -1,5 +1,6 @@
 'use client'
 import { Socket, io } from 'socket.io-client'
+import { ChatMessage } from '@/drizzle/schemas/chatMessages';
 
 // const socketServerURL = 'https://soei-socket-server.vercel.app/'
 const socketServerURL = 'http://localhost:3001/'
@@ -16,21 +17,56 @@ export async function connect() {
         }
     })
     socket.emit('login', id)
-    let data: any;
-    socket.on('users online', (data) => {
+    socket.on('users online', (data: [userId: string]) => {
         console.log('users online', data)
     })
-    socket.on('private message', (senderId, message) => {
-        console.log('Private message sent from:', senderId, message)
+    socket.on('private message', (senderId: string, chatMessage: ChatMessage) => {
+        console.log('Private message sent from:', senderId, chatMessage)
     })
-    socket.on('private message sent', (recipientId, message) => {
-        console.log('Private message sent to:', recipientId, message)
+    socket.on('private message sent', (recipientId: string, chatMessage: ChatMessage) => {
+        console.log('Private message sent to:', recipientId, chatMessage)
+    })
+    socket.on('group message', (chatroomId: string, chatMessage: ChatMessage, senderId: string) => {
+        console.log('Group message sent from:', senderId, 'in', chatroomId, chatMessage)
+    })
+    socket.on('group message sent', (chatroomId: string, chatMessage: ChatMessage, recipientId: string) => {
+        console.log('Group message sent to:', recipientId, 'in', chatroomId, chatMessage)
+    })
+    socket.on('join group', (chatroomId: string, joinUserId: string) => {
+        console.log('Join chatroom:', chatroomId, joinUserId)
+    })
+    socket.on('leave group', (chatroomId: string, leaveUserId: string) => {
+        console.log('Join chatroom:', chatroomId, leaveUserId)
     })
 }
 
 export function sendPrivateMessage(recipientId: Number, message: string) {
     if (socket) {
         socket.emit('private message', recipientId, message)
+    } else {
+        console.log('socket not found')
+    }
+}
+
+export function sendGroupMessage(chatroomId: Number, message: string) {
+    if (socket) {
+        socket.emit('group message', chatroomId, message)
+    } else {
+        console.log('socket not found')
+    }
+}
+
+export function joinGroupChatroom(chatroomId: Number) {
+    if (socket) {
+        socket.emit('join group', chatroomId)
+    } else {
+        console.log('socket not found')
+    }
+}
+
+export function leaveGroupChatroom(chatroomId: Number) {
+    if (socket) {
+        socket.emit('leave group', chatroomId)
     } else {
         console.log('socket not found')
     }
