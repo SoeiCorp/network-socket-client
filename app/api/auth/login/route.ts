7651b15c1d8c0c40978277cccd5598ab272bb238
SupanutT <supanut.tangsin@gmail.com>
2024-04-21 14:51:36 +0700

    fix : broken login route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,78 +6,38 @@ import { createToken } from "@/lib/auth";
 import { reqBody } from "./reqBody";
 
 export async function POST(req: NextRequest) {
-    const bcrypt = require('bcrypt')
-    try {
-        const reqBody: reqBody = await req.json();
-        const user = await db.select().from(users).where(eq(users.email, reqBody.email));
-        if (!user.length) {
-            return NextResponse.json({
-                success: false,
-                message: 'Wrong email or password'
-            }, { status: 404 })
-        }
-        const matchPassword = await bcrypt.compare(reqBody.password, user[0].password);
-        if (!matchPassword) {
-            return NextResponse.json({
-                success: false,
-                message: 'Wrong email or password'
-            }, { status: 404 })
-        }
-        const { token, cookieExpires } = await createToken(user[0])
-
-        return NextResponse.json({
-            success: true,
-            message: 'Successfully logged in',
-            id: user[0].id,
-            name: user[0].name
-        }, { status: 200, headers: { "Set-Cookie": `token=${token}; path=/; httpOnly=true; expires=${cookieExpires} ` } })
-
-    } catch (err) {
-        console.log(err)
-        return NextResponse.json({
-            success: false,
-            message: 'There is an error occurred',
-            error: err
-        }, { status: 400 })
-    }
-    const matchPassword = await bcrypt.compare(
-      reqBody.password,
-      user[0].password
-    );
-    if (!matchPassword) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Wrong email or password",
-        },
-        { status: 404 }
-      );
-    }
-    const { token, cookieExpires } = await createToken(user[0]);
-
-    return NextResponse.json(
-      {
-        success: true,
-        message: "Successfully logged in",
-        id: user[0].id,
-        name: user[0].name,
-      },
-      {
-        status: 200,
-        headers: {
-          "Set-Cookie": `token=${token}; Path=/; httpOnly=true; expires=${cookieExpires} `,
-        },
-      }
-    );
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json(
-      {
+  const bcrypt = require('bcrypt')
+  try {
+    const reqBody: reqBody = await req.json();
+    const user = await db.select().from(users).where(eq(users.email, reqBody.email));
+    if (!user.length) {
+      return NextResponse.json({
         success: false,
-        message: "There is an error occurred",
-        error: err,
-      },
-      { status: 400 }
-    );
+        message: 'Wrong email or password'
+      }, { status: 404 })
+    }
+    const matchPassword = await bcrypt.compare(reqBody.password, user[0].password);
+    if (!matchPassword) {
+      return NextResponse.json({
+        success: false,
+        message: 'Wrong email or password'
+      }, { status: 404 })
+    }
+    const { token, cookieExpires } = await createToken(user[0])
+
+    return NextResponse.json({
+      success: true,
+      message: 'Successfully logged in',
+      id: user[0].id,
+      name: user[0].name
+    }, { status: 200, headers: { "Set-Cookie": `token=${token}; path=/; httpOnly=true; expires=${cookieExpires} ` } })
+
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({
+      success: false,
+      message: 'There is an error occurred',
+      error: err
+    }, { status: 400 })
   }
 }
