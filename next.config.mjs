69f83a584900daf46f3051a7886d10dcf06d3f13
SupanutT <107112702+SupanutT@users.noreply.github.com>
2024-04-23 02:20:43 +0700

    Feat/skb frontend (#12)
    * fix : cors policy
    
    * fix : transports in socket connection

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,15 @@ const nextConfig = {
                     { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date' }
                 ]
             }
+            // {
+            //     source: '/',
+            //     headers: [
+            //         { key: 'Access-Control-Allow-Credentials', value: 'false' },
+            //         { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_SOCKET_SERVER_URL },
+            //         { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+            //         { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date' }
+            //     ]
+            // }
         ];
     }
 };
