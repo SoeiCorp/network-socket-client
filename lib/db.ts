3b2f9504c4f25e9d9b866d1f79c786142b2789c0
SupanutT <supanut.tangsin@gmail.com>
2024-04-23 22:18:53 +0700

    fix : revert back to using pool with config

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,15 @@
 import { Client, Pool, QueryResultRow } from 'pg'
 
-const globalForPg = globalThis as unknown as { pg: any }
-
-// Creates a global connection pool
-export const pg = globalForPg.pg || new Client({
+const globalPg = new Pool({
   connectionString: process.env.NEON_PG_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 })
+// Creates a global connection pool
+export const pg = globalPg
 
-if (process.env.NODE_ENV !== 'production') globalForPg.pg = pg
+// if (process.env.NODE_ENV !== 'production') globalForPg.pg = pg
 
 
 // export const query = <Result extends QueryResultRow>(
